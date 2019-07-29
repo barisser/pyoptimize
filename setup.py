c3548b188d24d16ef16b549b55f0479a0f96ec39
Andrew Barisser <barisser@protonmail.com>
2019-07-28 20:29:39 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyoptimize',
+    version='0.0.1',
+    author='Andrew Barisser',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        
+    ]
+    )
